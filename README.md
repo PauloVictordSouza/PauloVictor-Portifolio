cb3b63b238a22b004c87cbadb63e415d580bf3d5
Paulslagaah <163136883+Paulslagaah@users.noreply.github.com>
2024-08-08 09:44:36 -0300

    portifolio criado

---
+++ README.md
@@ -0,0 +1,2 @@
+# Portifiolio
+Esse é meu portifolio, nele irei apresentar projetos, experiencias profissionais, minhas habilidades e etc. Ele foi criado utilizando HTML5, CSS3 e JavaScript
---
+++ assets/js/acordeon.js
@@ -0,0 +1,17 @@
+const acordeonMove = document.querySelectorAll(".acordeon .trigger")
+
+acordeonMove.forEach((trigger) =>{
+
+    trigger.addEventListener("click", (e) =>{
+        const acordeon = trigger.parentElement
+        
+        const isOpen = acordeon.classList.contains("open")
+
+        if (isOpen) {
+           acordeon.classList.remove('open')
+        } else{
+            acordeon.classList.add('open')
+        }
+    })
+
+})
