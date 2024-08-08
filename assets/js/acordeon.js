79c41d6e1c376a2433e992b29ca0ff86eb2d4df9
PauloVictordSouza <darkkiller150706@gmail.com>
2024-08-08 18:23:00 -0300

    Corrijindo assets

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const acordeonMove = document.querySelectorAll(".acordeon .trigger")
 
 acordeonMove.forEach((trigger) =>{
@@ -15,22 +15,5 @@ acordeonMove.forEach((trigger) =>{
         }
     })
 
-=======
-const acordeonMove = document.querySelectorAll(".acordeon .trigger")
 
-acordeonMove.forEach((trigger) =>{
-
-    trigger.addEventListener("click", (e) =>{
-        const acordeon = trigger.parentElement
-        
-        const isOpen = acordeon.classList.contains("open")
-
-        if (isOpen) {
-           acordeon.classList.remove('open')
-        } else{
-            acordeon.classList.add('open')
-        }
-    })
-
->>>>>>> cb3b63b238a22b004c87cbadb63e415d580bf3d5
 })
--- assets/js/api.js
+++ assets/js/api.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
+
  async function fetchProfileData(){
-    const url = '/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/PauloVictordSouza/PauloVictor-Portifolio/main/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
-=======
- async function fetchProfileData(){
-    const url = '/data/profile.json'
-    const fetching = await fetch(url)
-    return await fetching.json()
->>>>>>> cb3b63b238a22b004c87cbadb63e415d580bf3d5
-}
+ }
